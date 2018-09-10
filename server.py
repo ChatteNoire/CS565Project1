5e157fbdb8abcfb26b57d3690930c0b88ff6aad0
Victoria <vv0011@uah.edu>
2018-09-09 19:09:49 -0500

    more commenting, try to make client write as w, not wb

--- server.py
+++ server.py
@@ -42,78 +42,72 @@ def serve():
     while True:
         conn, addr = s.accept()     # Establish connection with client, returns a tuple 
         print ('Got connection from', addr)
-        data = conn.recv(1024)      # 
+        data = conn.recv(1024)      # Receive from server
         print('Server received', repr(data))
 
-        #sympy.ntheory.generate.randprime(1000,999999)  # Generate large random prime in range [a,b)
-        p =  419 #sympy.randprime(200,9999)
-        print('p = ',p) #print out prime
-        g = 2 #sympy.randprime(2,1000)
-        # APPARENTLY g isn't random
-        # deal with eqn later g = math.exp((1%p)/(p-1))
-        print('g = ',g) #print out prime number g
+        # Agree on shared prime 'p' and shared key 'g'
+        p = sympy.randprime(200,9999)   # random prime generator for 'p'
+        print('p = ',p) # print out prime
+        g = sympy.randprime(2,1000)     # shared value g
+        # Lecture notes!  g = math.exp((1%p)/(p-1))
+        print('g = ',g) # print out prime number g
         
-        #conn.sendall(p.encode('utf-8'))
-        conn.sendall(p.to_bytes(16,'big')) #10 bytes? 
-        conn.sendall(g.to_bytes(16,'big'))
+        conn.sendall(p.to_bytes(16,'big'))  # send 'p' as bytes, 16 B, big endian
+        conn.sendall(g.to_bytes(16,'big'))  # send 'g' to client
 
-        p2 = 163 #sympy.randprime(2,2000)
-        print('p2 = ',p2)
+        # Generate secret prime 'p2'
+        p2 = sympy.randprime(2,2000)    # random prime 'p2'
+        while p2 == p:   # p2 != p
+            p2 = sympy.randprime(2,2000) # random 'p2' prime (secret)
+                
+        print('p2 = ',p2) # debugging p2 (only server knows)
+
+        # Generate public key Pk2 = g^(p2) mod p
         Pk2 = pow(g, p2, p) #pow(x,y[,z]) computers g^(y)mod(z) more efficiently
         print ('Pk2 = ',Pk2)
 
         # Receive Pk1 from Client (1)
-        Pk1 = conn.recv(1024)
-        Pk1 = int.from_bytes(Pk1, byteorder='big')
+        Pk1 = conn.recv(1024)   
+        Pk1 = int.from_bytes(Pk1, byteorder='big')  # convert Pk1 bytes -> int
         print('Pk1 = ',Pk1)
 
         # Send Pk2 to Client (2)
-        conn.sendall(Pk2.to_bytes(10,'big'))
+        conn.sendall(Pk2.to_bytes(16,'big'))
 
-        # Get Client Shared Key
+        # Get Client Shared Key (3)
         Pk1_p2 = pow(Pk1, p2)
-        Pk1_p2 = Pk1_p2 % p
-        print('Shared DH key: ', Pk1_p2)
+        Pk1_p2 = Pk1_p2 % p     # Pk1_p2 = Pk1^p2 % p
+        print('Shared DH key: ', Pk1_p2) # should match with client's Pk2_p1
         
         # file to transfer
         filename='sample.txt'
-        f = open(filename,'r') #formerly rb
-        #l = f.read(1024)
-        l = f.read()
-        l = str.encode(l)
-        l = l.rjust(32)
+        f = open(filename,'r') #formerly rb from template (read all at once)
+        l = f.read()    # extract contents of file
+        l = l.rjust(32) # right justified, width of string
+        l = str.encode(l)   # l as a string to bytes
         print('read: ',l)
 
-        #debugging
-        #l = b'hi there'.rjust(32)
+        # debugging 
+        #l = b'Hi there'.rjust(32)
         
         # Encrypt file
-        Pk1_p2 = Pk1_p2.to_bytes(16,'big')
+        Pk1_p2 = Pk1_p2.to_bytes(16,'big') # convert shared key to bytes
         print(Pk1_p2)
-        #cipher_aes = AES.new(Pk1_p2, AES.MODE_EAX)
-        #ciphertext = cipher_aes.encrypt(l.encode("utf-8").strip())
-        
-        cipher = AES.new(Pk1_p2, AES.MODE_ECB)
-        ciphertext = base64.b64encode(cipher.encrypt(l))
+        # AES encryption with key
+        cipher = AES.new(Pk1_p2, AES.MODE_ECB) 
+        ciphertext = base64.b64encode(cipher.encrypt(l)) # encrypt 'l' file text
         print('Ciphertext: ',ciphertext)
-
-        #debugging
-        #ciphertext = b'abcd'
         
         # Implement MD5 hash on Ciphertext
         md5hash = hashlib.md5(ciphertext).hexdigest()
         print(md5hash)
         
-        #while (l):
-        conn.sendall(ciphertext) #conn.sendall(l) #send -> sendall b/c Python3
-        print('Sent ',repr(ciphertext)) # formerly l
-            #l = f.read(1024)
-            #print('read in loop: ',l)
-        f.close()
+        conn.sendall(ciphertext)  #send -> sendall b/c Python3
+        print('Sent ',repr(ciphertext)) # print ciphertext sent to client
+        f.close()   # close input file
         print('Done sending')
-        # This line screws it up
-        # conn.send(b'File transfer complete!')
-        conn.close()
+        conn.send(b'File transfer complete!')
+        conn.close() # close socket connection
 
 if __name__=='__main__':
     serve()
